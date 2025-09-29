765ad76ede4e589d03a5d4f8fb9bbee95e05e61d
SodiumPhosphate24 <daniel.jingyu.wang@gmail.com>
2025-09-29 19:57:53 +0000

    Enemy Damage

--- enemy.js
+++ enemy.js
@@ -23,6 +23,10 @@ class Enemy {
   isDead() {
     return this.health <= 0;
   }
+
+  hitsPlayer(){
+    return checkCollision(this.x, this.y, pX + 600 + camX, pY + 340 + camY, 20, 20, 35, 25);
+  }
 }
 
 function drawEnemies() {
@@ -30,6 +34,15 @@ function drawEnemies() {
     enemies[i].update();
     fill(255, 0, 0);
     rect(enemies[i].x, enemies[i].y, 20, 20);
+    if (enemies[i].isDead()) {
+      enemies.splice(i, 1);
+      i--;
+    }
+    if (enemies[i].hitsPlayer()){
+      if (enemies[i].type == "zombie"){
+        healthPoints -= 2;
+      }
+    }
   }
 }
 
