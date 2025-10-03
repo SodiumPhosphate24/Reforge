b5d24001e93cbadc549b8f91a9fa2851feade6bb
SodiumPhosphate24 <daniel.jingyu.wang@gmail.com>
2025-10-03 08:35:54 -0600

    Update bullet.js

--- bullet.js
+++ bullet.js
@@ -3,9 +3,9 @@ class Bullet {
     this.x = pX + 600;
     this.y = pY + 375;
     this.angle = atan2(mouseY - (this.y + camY), mouseX - (this.x + camX));
-    if (type = "common") {
+    if (type == "common") {
       this.type = "common";
-      this.image = 0;
+      this.image = BulletImgs[0];
       this.speed = 35;
       this.damage = 1;
 
@@ -32,25 +32,41 @@ class Bullet {
     return checkTileCollisions(this.x, this.y, 20, 20);
   }
 }
-
 function drawBullets() {
+  let count = 0;
   for (let i = 0; i < bullets.length; i++) {
-    bullets[i].update();
-    fill(255, 0, 0);
-    rotate(bullets[i].angle);
-    image(BulletImgs[this.image], bullets[i].x, bullets[i].y, 20, 20)
-    if (bullets[i].hitsEnemy() || bullets[i].hitsWall()) {
-      bullets.splice(i, 1);
-      i--;
+    const b = bullets[count];
+    b.update();
+
+    push();
+      // move to bullet’s world position
+      translate(b.x, b.y);
+      // rotate around its center
+      rotate(b.angle);
+
+      // draw the bullet image (or fallback rect) centered
+      if (b.image) {
+        image(b.image, -10, -10, 20, 20); // -10,-10 centers it
+      } else {
+        fill(255, 0, 0);
+        rect(-10, -10, 20, 20);
+      }
+    pop();
+
+    if (b.hitsEnemy() || b.hitsWall()) {
+      bullets.splice(count, 1);
+      count--;
     }
+    count--;
   }
 }
+
 
 
 function calculateAim() {
   // player center in SCREEN space
   const pxs = pX + camX + pWidth / 2 + 600;
-  const pys = pY + camY + pHeight / 2+370;
+  const pys = pY + camY + pHeight / 2 + 370;
 
   return atan2(mouseY - pys, mouseX - pxs);
 }
