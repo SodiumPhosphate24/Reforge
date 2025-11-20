a2e0b2e855976c67649e04a5177ffe9de69bb19c
SodiumPhosphate24 <daniel.jingyu.wang@gmail.com>
2025-11-20 02:54:19 +0000

    Testing Trigger

--- trigger.js
+++ trigger.js
@@ -8,9 +8,11 @@ var triggerList = {
 };
 
 function handleTriggers(trigger){
+  console.log("handling " + trigger);
   if (trigger == "Prometheus"){
     if (triggerList.Promethus.talkToPrometheus == false){
       triggerList.Promethus.talkToPrometheus = true;
+      console.log("Talked to Prometheus");
       droppedItems.push(new DroppedItem(new Item("gun", "glock", 1), 13350, 12750));
       triggerList.Promethus.dropppedStarterGun = true;
     }
